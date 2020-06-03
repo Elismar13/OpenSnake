baeeeac1cf74291b98688b57d4494fa023d3c3fa
Elismar13 <elismarsilva.5@outlook.com>
2020-06-03 14:52:43 -0300

    fix: 🐛 Grid class fixed

--- src/game.cpp
+++ src/game.cpp
@@ -1,41 +1,33 @@
 #include <GL/glut.h>
 #include <GL/gl.h>
 
-#include "game.h"
+#include "game.hpp"
 
-class Grid {
-    private:
-        int gridX, gridY;
 
-    public:
-        Grid() {
-            
+void Grid :: initGrid(int x, int y) {
+    this->gridX = x;
+    this->gridY = y;
+}
+
+void Grid :: gridUnit(int x, int y) {
+    glLineWidth(1.0);
+    glColor3f(1.0, 1.0, 1.0);
+
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(x, y);
+        glVertex2f(x+1, y);
+        glVertex2f(x+1, y+1);
+        glVertex2f(x, y+1);
+    glEnd();
+}
+
+void Grid :: drawGrid() {
+    for(int x = 0; x < this->gridX; x++) {
+        for(int y = 0; y < this->gridY; y++) {
+            this->gridUnit(x, y);
         }
+    }
+}
 
-        void initGrid(int x, int y) {
-            this->gridX = x;
-            this->gridY = y;
-        }
-
-        void gridUnit(int x, int y) {
-            glLineWidth(1.0);
-            glColor3f(1.0, 1.0, 1.0);
-
-            glBegin(GL_LINE_LOOP);
-                glVertex2f(x, y);
-                glVertex2f(x+1, y);
-                glVertex2f(x+1, y+1);
-                glVertex2f(x, y+1);
-            glEnd();
-        }
-
-        void drawGrid() {
-            for(int x = 0; x < this->gridX; x++) {
-                for(int y = 0; y < this->gridY; y++) {
-                    this->gridUnit(x, y);
-                }
-            }
-        }
-};
 
 
--- src/game.hpp
+++ src/game.hpp
@@ -1,2 +1,20 @@
-#ifndef "game.h"
-    #define "game.h"
+#ifndef GAME_HPP
+#define GAME_HPP
+
+class Grid {
+    private:
+        int gridX, gridY;
+
+    public:
+        //Grid();
+
+        void initGrid(int x, int y);
+
+        void gridUnit(int x, int y);
+
+        void drawGrid();
+};
+
+#endif
+
+
