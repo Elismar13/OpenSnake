b05ef856c005616342118a7aecea029048acea55
Elismar13 <elismarsilva.5@outlook.com>
2020-06-15 18:37:25 -0300

    fix: 🐛 Desenhando o corpo completo + sistema de pontuaçao

--- src/Snake.cpp
+++ src/Snake.cpp
@@ -9,25 +9,41 @@ using namespace std;
 
 Snake :: Snake () {
     setSnakePosition(20, 20);
+    setInitialPoints(5);
     snakeDirection = RIGHT;
+    this->gameOver = false;
 }
-        
+
+SnakeBody Snake :: getSnakeHeader() {
+    return this->body[0];
+}
+bool Snake :: endGame() {
+    this->gameOver = true;
+}
+
+bool Snake :: isOver() {
+    return this->gameOver;
+}
+
+
 bool Snake :: setSnakePosition(int positionX, int positionY) {
-    if(positionX >= 1 && positionX < 39 && positionY >= 1 && positionY < 39) {
-        this->positionX = positionX;
-        this->positionY = positionY;
+
+    for (int i = 2; i < getPoints(); i++) {
+        if(this->body[0].posX == this->body[i].posX && this->body[0].posY == this->body[i].posY )
+            this->endGame();
+    }
+
+    if(positionX > 0 && positionX < 39 && positionY > 0 && positionY < 39) {
+        this->body[0].posX = positionX;
+        this->body[0].posY = positionY;
         return true;
     } 
-    else return false;
+    else {
+        //cout << positionY << ' ' << positionX << endl;
+        this->endGame();
+        return false;
+    }
 }  
-
-int Snake :: getSnakePositionX() {
-    return this->positionX;
-}
-
-int Snake :: getSnakePositionY() {
-    return this->positionY;
-}
 
 short int Snake :: getSnakeDirection() {
     return this->snakeDirection;
@@ -38,10 +54,27 @@ void Snake :: setSnakeDirection(Direction direction) {
         this->snakeDirection = direction;
 }
 
+void Snake :: setInitialPoints(int initialPoints) {
+    this->points = initialPoints;
+}
+
+void Snake :: incrementPoints() {
+    this->points++; // pontos += 1 
+}
+
+unsigned int Snake :: getPoints() {
+    return this->points;
+}
+
 void Snake :: drawSnake() {
     const short int direction = (const short) this->getSnakeDirection();
-    int posX = this->getSnakePositionX();
-    int posY = this->getSnakePositionY();
+    int posX = this->body[0].posX;
+    int posY = this->body[0].posY;
+
+    for (int i = getPoints(); i > 0; i--) {
+        // i for 1
+        this->body[i] = this->body[i-1];
+    }
 
     switch (direction)
     {
@@ -65,10 +98,15 @@ void Snake :: drawSnake() {
             break;
     }
 
-        // cout << "Posicao X: " << posX << endl;
-        // cout << "Posicao Y: " << posY << endl;
+    for (int snakeIndex = 0; snakeIndex < points; snakeIndex++) {
+        SnakeBody position = this->body[snakeIndex];
+        if(snakeIndex <= 1) 
+            glColor3f(0.1, 0.485, 0.9);
+        else 
+            glColor3f(0.45, 0.45, 0.45);
+        glRectd(position.posX, position.posY, position.posX+1, position.posY+1);
+    }
 
-    glRectd(posX, posY, posX+1, posY+1);
 
     bool Collider = setSnakePosition(posX, posY);
 }
--- src/Snake.hpp
+++ src/Snake.hpp
@@ -8,22 +8,38 @@ enum Direction {
     RIGHT = 4,
 };
 
+typedef struct {
+    int posX;
+    int posY;
+} SnakeBody;
+
 class Snake {
     private:
-        int positionX, positionY;
+        bool gameOver;
         short int snakeDirection;
+        unsigned int points;
+        SnakeBody body[50];
+
     public:
         Snake();
         
+        SnakeBody getSnakeHeader();
+
+        bool endGame();
+
+        bool isOver();
+
         bool setSnakePosition(int positionX, int positionY);
-
-        int getSnakePositionX();
-
-        int getSnakePositionY();
 
         short int getSnakeDirection();
 
         void setSnakeDirection(Direction);
+
+        void setInitialPoints(int initialPoints);
+
+        void incrementPoints();
+
+        unsigned int getPoints();
 
         void drawSnake();
 };
